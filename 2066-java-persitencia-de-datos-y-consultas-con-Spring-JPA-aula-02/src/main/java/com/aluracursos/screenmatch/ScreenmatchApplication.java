b0deeaff856cdfb4c641b26a478a080d556ba796
Juandmj82 <juandiegosdb@gmail.com>
2024-10-25 20:43:03 -0500

    Agrego todos los archivos del proyecto Spring JPA

---
+++ 2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-aula-02/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,34 @@
+package com.aluracursos.screenmatch;
+
+import com.aluracursos.screenmatch.principal.Principal;
+import com.aluracursos.screenmatch.repository.SerieRepository;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	@Autowired
+	private SerieRepository repository;
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("API_KEY", dotenv.get("API_KEY")); // Cargar la API key
+
+
+		SpringApplication.run(ScreenmatchApplication.class, args);//cargar las variables de entorno desde el archivo .env
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repository);
+		principal.muestraElMenu();
+	}
+}
